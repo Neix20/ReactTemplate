82fd5daec828e619734d17a78c9d19128ca28f6b
Neix20 <txen2000@gmail.com>
2025-05-12 20:32:28 +0800

    feat: Update

--- src/app/Routes/index.jsx
+++ src/app/Routes/index.jsx
@@ -5,6 +5,8 @@ import AdminRoutes from './AdminRoutes';
 import UserRoutes from "./UserRoutes";
 import NoLayoutRoutes from "./NoLayoutRoutes";
 
+// Tst
+
 // ==============================|| ROUTING RENDER ||============================== //
 
 // const router = createBrowserRouter([AdminRoutes, UserRoutes], { basename: import.meta.env.VITE_APP_BASE_NAME });
